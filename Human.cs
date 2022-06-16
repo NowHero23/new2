5ef5dca12be982c402093e4c479558c6c43e0f34
NowHero23 <lev4encko.san.2033@gmail.com>
2022-06-16 21:00:37 +0300

    add Human

---
+++ Human.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pv121_git
+{
+    class Human
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+    }
+}
