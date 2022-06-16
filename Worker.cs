7c555e2ea7d8c597ec290571add682bd03fc66ea
NowHero23 <lev4encko.san.2033@gmail.com>
2022-06-16 21:03:17 +0300

    add Worker

---
+++ Worker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pv121_git
+{
+    class Worker :Human
+    {
+        double Salary { get; set; }
+    }
+}
